333cdd999f2cd4d046c1958e3df5352620b2d5ce
Siddhanth Jagtap <sid.jagtap2003@gmail.com>
2021-12-02 20:02:42 +0530

    spacing

--- Assignment - 2/FirstLastSwap.py
+++ Assignment - 2/FirstLastSwap.py
@@ -1,13 +1,13 @@
 #Program to Interchange First and Last List Elements
 def myfunc(List1):
     print("Name: Siddhanth Jagtap")
-    print("\n List: ", List1)
+    print("\nList: ", List1)
     length = len(List1)
     #Swap first element with last element using temporary variable
     t = List1[0]
     List1[0] = List1[length -1]
     List1[length-1] = t
-    print("\n List After Interchanging First and Last List Elements", List1)
+    print("\nList After Interchanging First and Last List Elements", List1)
 
 List1=[1,3,5,7,9]
 myfunc(List1)
