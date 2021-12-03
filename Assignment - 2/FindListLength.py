01b153fa06ba99881dddef73dfcd72cd2cbb1edb
Siddhanth Jagtap <sid.jagtap2003@gmail.com>
2021-12-03 20:38:59 +0530

    q3

--- Assignment - 2/FindListLength.py
+++ Assignment - 2/FindListLength.py
@@ -1,12 +1,12 @@
 #Program to Find Length of a List
 def myfunc(List1):
-    print("Name: Siddhanth Jagtap")
+    print("\nName: Siddhanth Jagtap")
     len1 = len(List1)
     len2 = 0
     for i in List1:
         len2+=1
-    print("Length Using len()=",len1)
-    print("Length Using Counter=",len2)
-    
+    print("\nLength Using len()=",len1)
+    print("\nLength Using Counter=",len2)
+    print()
 List1 = [1,3,5,7,9]
 myfunc(List1)
