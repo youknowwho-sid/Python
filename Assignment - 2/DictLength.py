98167fef1a299dcca088da4247cdd0ebd028848b
Siddhanth Jagtap <sid.jagtap2003@gmail.com>
2021-12-08 00:40:15 +0530

    q20

---
+++ Assignment - 2/DictLength.py
@@ -0,0 +1,9 @@
+#Python program to find length of the dictionary
+def myfunc():
+    print("\nName: Siddhanth Jagtap")
+    D1 ={'A':1, 'B':2, 'C':3}
+    print("\nlen() method :", len(D1))
+    print("\nlen() method with keys() :", len(D1.keys()))
+    print("\nlen() method with values():", len(D1.values()))
+    print()
+myfunc()
