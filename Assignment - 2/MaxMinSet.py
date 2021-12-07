ff0bed7a3d1e2a612b8ad0f6af82b1e423c9b7a4
Siddhanth Jagtap <sid.jagtap2003@gmail.com>
2021-12-08 01:39:30 +0530

    q23

---
+++ Assignment - 2/MaxMinSet.py
@@ -0,0 +1,8 @@
+# Python code to get the maximum and minimum element from a set
+def myfunc():  
+    print("\nName: Siddhanth Jagtap")
+    sets = set([8, 16, 24, 1, 25, 3, 10, 65, 55])
+    print("\nMax = ",max(sets))
+    print("\nMin = ",min(sets))
+    print()
+myfunc()
