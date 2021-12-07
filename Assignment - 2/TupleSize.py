59f9268000ce8c3fb457a9c315701f44c83d53ab
Siddhanth Jagtap <sid.jagtap2003@gmail.com>
2021-12-07 22:36:09 +0530

    q11

--- Assignment - 2/TupleSize.py
+++ Assignment - 2/TupleSize.py
@@ -3,6 +3,7 @@ import sys
 def myfunc():
     print("\nName: Siddhanth Jagtap")
     T1 = ("V", "I", "T", "A", "P")
+    print("\nLength of Tuple: " + str(len(T1)))
     print("\nSize of Tuple: " + str(sys.getsizeof(T1)) + " bytes")
     print()
 myfunc()
