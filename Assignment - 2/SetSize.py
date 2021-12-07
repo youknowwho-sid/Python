48f32831ad8ad9dc7b91457dc7a3d5e402ef5bd7
Siddhanth Jagtap <sid.jagtap2003@gmail.com>
2021-12-08 01:03:32 +0530

    q21

---
+++ Assignment - 2/SetSize.py
@@ -0,0 +1,7 @@
+#Program to Find the Size of a Set
+def myfunc():
+    print("\nName: Siddhanth Jagtap")
+    n = len({1, 2, 3, 4, 5})
+    print("\nThe length of set is:", n)
+    print()
+myfunc()
