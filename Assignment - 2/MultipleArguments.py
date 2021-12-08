329f3af340144d52b395fec218a549003ef5a267
Siddhanth Jagtap <sid.jagtap2003@gmail.com>
2021-12-08 22:55:15 +0530

    q27

---
+++ Assignment - 2/MultipleArguments.py
@@ -0,0 +1,8 @@
+#How to Print Multiple Arguments in Python?
+print("\nName: Siddhanth Jagtap")
+def myfunc(k,l):
+    print("\nInput by user = ",k,",",l)
+    print()
+a = input("\nEnter Input 1: ")
+b = input("Enter Input 2: ")
+myfunc(a,b)
