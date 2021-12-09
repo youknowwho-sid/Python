550d3878a94fa45316521370b7bab5899a2db3b4
Siddhanth Jagtap <sid.jagtap2003@gmail.com>
2021-12-10 01:34:51 +0530

    q30

---
+++ Assignment - 2/ReadWriteFile.py
@@ -0,0 +1,12 @@
+#Read content from one file and write it into another file
+with open(r"D:\Personal\Python\Assignment - 2\text.txt","r") as h:
+    print(h.read())
+
+    text2 = open(r"D:\Personal\Python\Assignment - 2\text2.txt)", "w")
+
+    with open(r"D:\Personal\Python\Assignment - 2\text.txt", "r") as g:
+        text2.write(g.read())
+    text2.close()
+
+with open(r"D:\Personal\Python\Assignment - 2\text2.txt","r") as i:
+    print(i.read())
