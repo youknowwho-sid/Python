acacac29b5c5aca4468928f015d49efbdbec5f22
Siddhanth Jagtap <92534771+youknowwho-sid@users.noreply.github.com>
2024-02-27 12:55:29 +0530

    Update 2.py

--- Assignment - 5 Lab/2.py
+++ Assignment - 5 Lab/2.py
@@ -4,7 +4,7 @@ def myfunc():
     n = 1                           #we have to go till n = A + B = 7 + 4 = 11
     a = [1,]                        #making a list of all the an elements
     while(n<12):
-        if(a[n-1] - n > 0):
+        if(a[n-1] - n > 0): 
             a.append(a[n-1] - n)    #appending to the list
         else:
             a.append(a[n-1] + n)
